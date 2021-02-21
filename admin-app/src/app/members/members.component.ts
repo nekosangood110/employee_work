329b2d896049d81c969222cdb557c19242526e3c
LAPTOP-SAHND00I\asuha <asuha2314@gmail.com>
2021-02-21 13:12:41 +0900

    components

--- admin-app/src/app/members/members.component.ts
+++ admin-app/src/app/members/members.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member'; //.ts要らない
 import { MEMBERS } from '../mock-members';
+
+
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
@@ -8,18 +10,25 @@ import { MEMBERS } from '../mock-members';
 })
 export class MembersComponent implements OnInit {
 
-  members = MEMBERS;  
+  members = MEMBERS;
+
   member: Member = {　//このMemberプロパティにMemberInterfaceを型として指定
 
     id: 1,
     name: '佐藤　秋'
   
   };
-  
+  selectedMember: Member;
   
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(member: Member): void  {
+    this.selectedMember = member;
+  }
+
+
 }
+
